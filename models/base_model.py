f1c0af89312b87beda2052d366869913afce003d
kenkomu <komukenneth216@gmail.com>
2022-09-04 10:51:00 +0300

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,7 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
 """
 base_model module
 """
-from datetime import datetime
-import uuid
-time = "%Y-%m-%dT%H:%M:%S.%f"
-class BaseModel:
-    """
-    class BaseModel
-    """
-    id = primary_key=True
-    created_at = DateTime, default=datetime.utcnow
-    updated_at = DateTime, default=datetime.utcnow
-    def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
-    def __str__(self):
-        """String representation of the BaseModel class"""
-        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-                                         self.__dict__)
-=======
-"""Module for Base class
-Contains the Base class for the AirBnB clone console.
-"""
-
 import uuid
 from datetime import datetime
 class BaseModel:
@@ -54,4 +32,4 @@ class BaseModel:
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
         return my_dict
->>>>>>> 31e26df4c17d26f4692c01000ff1441978eb470d
+
