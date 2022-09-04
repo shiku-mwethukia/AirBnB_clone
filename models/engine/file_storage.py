26dd03f581e8759e220afb4a2166cc49cc0835aa
kenkomu <komukenneth216@gmail.com>
2022-09-04 11:47:00 +0300

    engine/

---
+++ models/engine/file_storage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+file_storage module
+"""
+import os
+import json
+class FileStorage:
+    """class FileStorage"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns __objects dictionary."""
+        # TODO: should this be a copy()?
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Sets new obj in __objects dictionary."""
+        # TODO: should these be more precise specifiers?
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        with open(FileStorage.__file_path,"w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, f)
+
+    def reload(self):
+        """Deserializes JSON file into __objects."""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
