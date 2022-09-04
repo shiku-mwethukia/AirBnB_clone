04dcbaa652b0aa9c50ef3dff23a6c799f20c67ae
kenkomu <komukenneth216@gmail.com>
2022-09-04 14:57:44 +0300

    console.py

--- console.py
+++ console.py
@@ -2,8 +2,26 @@
 """console module"""
 
 import cmd, sys
+import re
+from shlex import split
 class HBNBCommand(cmd.Cmd):
     """class HBNBCommand"""
+    def parse(arg):
+        curly_braces = re.search(r"\{(.*?)\}", arg)
+        brackets = re.search(r"\[(.*?)\]", arg)
+        if curly_braces is None:
+            if brackets is None:
+                return [i.strip(",") for i in split(arg)]
+            else:
+                lexer = split(arg[:brackets.span()[0]])
+                retl = [i.strip(",") for i in lexer]
+                retl.append(brackets.group())
+                return retl
+        else:
+            lexer = split(arg[:curly_braces.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(curly_braces.group())
+            return retl
 
     def do_quit(self, arg):
         """Quit command to exit the program."""
