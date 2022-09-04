26fb35906a89e313798da0898073814382c940e3
kenkomu <komukenneth216@gmail.com>
2022-09-04 16:37:03 +0300

    console.py

--- console.py
+++ console.py
@@ -6,11 +6,11 @@ from shlex import split
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
-# from models.state import State
-# from models.city import City
-# from models.place import Place
-# from models.amenity import Amenity
-# from models.review import Review
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 def parse(arg):
@@ -41,11 +41,11 @@ class HBNBCommand(cmd.Cmd):
     __classes = {
         "BaseModel",
         "User",
-        # "State",
-        # "City",
-        # "Place",
-        # "Amenity",
-        # "Review"
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
     }
 
     def emptyline(self):
