75669ff26f0365c94ee7d2d5be12efd022640e25
kenkomu <komukenneth216@gmail.com>
2022-09-04 11:21:19 +0300

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -7,10 +7,6 @@ from datetime import datetime
 class BaseModel:
 
     """Class for base model of object hierarchy."""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
             
     def __str__(self):
         """Returns a human-readable string representation
