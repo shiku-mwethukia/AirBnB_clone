aa00d5198b7cabed2766b30c30ea6adb6d964271
kenkomu <komukenneth216@gmail.com>
2022-09-04 16:23:16 +0300

    changes

--- models/place.py
+++ models/place.py
@@ -4,8 +4,8 @@ from models.base_model import BaseModel
 
 class Place(BaseModel):
     name = ""
-    city_id = "City.id"
-    user_id =  "User.id"
+    city_id = ""
+    user_id =  ""
     name = ""
     description = ""
     number_rooms = 0
@@ -14,4 +14,4 @@ class Place(BaseModel):
     price_by_night = 0
     latitude = 0.0
     longitude = 0.0
-    amenity_ids = ["Amenity.id"]
+    amenity_ids = [""]
