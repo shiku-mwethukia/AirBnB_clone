a851721b9587e60f1be57f520460f1e80d05831d
kenkomu <komukenneth216@gmail.com>
2022-09-04 09:31:12 +0300

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,13 +5,16 @@ base_model module
 from datetime import datetime
 import uuid
 time = "%Y-%m-%dT%H:%M:%S.%f"
+
+
 class BaseModel:
     """
     class BaseModel
     """
-    id = primary_key=True
-    created_at = DateTime, default=datetime.utcnow
-    updated_at = DateTime, default=datetime.utcnow
-    def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
+    id = primary_key = True
+    created_at = DateTime, default = datetime.utcnow
+    updated_at = DateTime, default = datetime.utcnow
 
+    def __str__(self):
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
