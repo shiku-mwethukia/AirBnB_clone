95db963f74ba60bc9743267079e30263799877f5
kenkomu <komukenneth216@gmail.com>
2022-09-04 15:52:55 +0300

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -28,6 +28,25 @@ class FileStorage:
             d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(d, f)
 
+    def classes(self):
+        """Returns a dictionary of valid classes and their references."""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
     def reload(self):
         """Deserializes JSON file into __objects."""
         if not os.path.isfile(FileStorage.__file_path):
@@ -50,26 +69,29 @@ class FileStorage:
                      {"email": str,
                       "password": str,
                       "first_name": str,
-                      "last_name": str}
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
         }
         return attributes
-    def classes(self):
-        """Returns a dictionary of valid classes and their references."""
-        from models.base_model import BaseModel
-        from models.user import User
-        # from models.state import State
-        # from models.city import City
-        # from models.amenity import Amenity
-        # from models.place import Place
-        # from models.review import Review
-
-        classes = {"BaseModel": BaseModel,
-                   "User": User,
-                #    "State": State,
-                #    "City": City,
-                #    "Amenity": Amenity,
-                #    "Place": Place,
-                #    "Review": Review}
-        }
-        return classes
-            
