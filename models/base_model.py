743395a47ae45857a111932e214f527801d504f0
kenkomu <komukenneth216@gmail.com>
2022-09-04 11:57:34 +0300

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ base_model module
 """
 import uuid
 from datetime import datetime
+from engine.__init__ import storage
 class BaseModel:
 
     """Class for base model of object hierarchy."""
@@ -20,6 +21,8 @@ class BaseModel:
         with the current datetime."""
 
         self.updated_at = datetime.now()
+        storage.save()
+
     def to_dict(self):
         """Returns a dictionary representation of an instance."""
 
@@ -49,3 +52,4 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
