36fce87c5998dc32af0df4a558e44298fb6a9d7a
kenkomu <komukenneth216@gmail.com>
2022-09-04 16:07:32 +0300

    classes

---
+++ models/city.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for city class."""
+from models.base_model import BaseModel
+
+class City:
+    name = ""
+    state_id = "State.id"
---
+++ models/review.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for review class."""
+from models.base_model import BaseModel
+
+class Review:
+    place_id = "Place.id"
+    user_id = "User.id"
+    text = ""
